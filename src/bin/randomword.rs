d5181efa729506007295246a03e0b9e614396858
Max <d3vboi@proton.me>
2023-09-04 07:19:23 +0200

    changed snakecase problems

--- src/bin/randomword.rs
+++ src/bin/randomword.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
-use Rustypex::textgen::{RawWordSelector, WordSelector};
-use Rustypex::wordlists::OS_WORDLIST_PATH;
+use rustypex::textgen::{RawWordSelector, WordSelector};
+use rustypex::wordlists::OS_WORDLIST_PATH;
 
 fn main() {
     let mut word_selector = RawWordSelector::from_path(PathBuf::from(OS_WORDLIST_PATH)).unwrap();
